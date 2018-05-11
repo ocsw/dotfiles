e916fe4f4c00f7636912343da6eac296cb452451
Danielle Zephyr Malament <danielle.malament@gmail.com>
2018-05-11 00:47:25 -0400

    errorcode_prompt improvements

--- dot.bashrc.d/errorcode_prompt.post.sh
+++ dot.bashrc.d/errorcode_prompt.post.sh
@@ -1,16 +1,25 @@
 _errorcode_prompt () {
-  # without this, and with local, there's an extra space in the output;
-  # evidently some interaction between local and implicit arrays
-  #typeset -a PIPESTATUS_SIG
-  #local i signame PIPESTATUS_SIG
-  for i in "${PIPESTATUS[@]}"; do
-    if [[ "$i" -gt 128 ]]; then
-      signame=$(builtin kill -l $(($i - 128)) 2>/dev/null)
+  # first copy the status array in case we do something that changes it as we
+  # go along
+  local pipestatus_num=("${PIPESTATUS[@]}")
+
+  local stat_num signame pipestatus_sig
+  # without this, and with local, the array gets a null element 0;
+  # order doesn't matter
+  typeset -a pipestatus_sig
+
+  for stat_num in "${pipestatus_num[@]}"; do
+    if [[ "$stat_num" -gt 128 ]]; then
+      signame=$(builtin kill -l $(($stat_num - 128)) 2>/dev/null)
       signame="${signame:3}"
-      PIPESTATUS_SIG[${#PIPESTATUS_SIG[*]}]="$i ($signame)"
+      pipestatus_sig=("${pipestatus_sig[@]}" "$stat_num ($signame)")
     else
-      PIPESTATUS_SIG[${#PIPESTATUS_SIG[*]}]="$i"
+      # this would be better but less backwards-compatible:
+      # pipestatus_sig+=("$stat_num")
+      # see https://stackoverflow.com/questions/1951506/add-a-new-element-to-an-array-without-specifying-the-index-in-bash
+      pipestatus_sig=("${pipestatus_sig[@]}" "$stat_num")
     fi
   done
-  printf "%s" "${PIPESTATUS_SIG[*]}"
+
+  printf "%s" "${pipestatus_sig[*]}"
 }
