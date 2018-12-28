639d9ee642a013f6896efd9ce81ee50abfcaf129
Danielle Zephyr Malament <danielle.malament@gmail.com>
2018-12-27 20:52:45 -0500

    add update-repos

--- dot.bashrc.d/git.post.sh
+++ dot.bashrc.d/git.post.sh
@@ -27,8 +27,36 @@ clone-fork () {
 
 update-fork () {
     git fetch upstream || return $?
-    git co master || return $?
+    git checkout master || return $?
     git pull || return $?
     git merge upstream/master || return $?
     git push
+}
+
+update-repos () {
+    start_dir="$PWD"
+    trap 'cd "$start_dir"' RETURN
+
+    own_repos="
+        .dotfiles
+        .pypvutil
+        sysadmin-notes
+    "
+    cd "${HOME}" || return $?
+    for i in $own_repos; do
+        cd "$i" || continue
+        if [ -z "$(git status --porcelain)" ]; then
+            git pull
+            git push
+        fi
+        cd - || return $?
+    done
+
+    # vim stuff
+    cd "${HOME}/.vim" || return $?
+    for i in vim-pathogen bundle/*; do
+        cd "$i" || continue
+        git pull
+        cd - || return $?
+    done
 }
