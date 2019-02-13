902c049e4266c1ca840d19c26eb407a67b4d7717
Danielle Zephyr Malament <danielle.malament@gmail.com>
2019-02-12 22:26:01 -0500

    brew wrapper

--- dot.bashrc.d/brew.post.sh
+++ dot.bashrc.d/brew.post.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 
-brew-fix-perms () {
-    local gw="
-        /usr/local/bin
-        /usr/local/include
-        /usr/local/sbin
-        /usr/local/etc
-        /usr/local/Homebrew
-        /usr/local/var
-        /usr/local/var/homebrew
-        /usr/local/var/homebrew/linked
-        /usr/local/lib
-        /usr/local/opt
-        /usr/local/Frameworks
-        /usr/local/share
-    "
-    # shellcheck disable=SC2086
-    chmod 775 $gw
-    chmod -R go=u-w /usr/local
-    find /usr/local \! -perm 444 -a \! -perm 555 -a \! -perm 644 -a \
-        \! -perm 755 -a \! -perm 775 -print0 | \
-        xargs -0 chmod -h go=u-w
-}
+if in_path brew; then
+    brew () {
+        local reset_umask
+        local real_brew
+        reset_umask=$(umask -p)
+        # shellcheck disable=SC2230
+        real_brew=$(which brew)
+        umask 022
+        "$real_brew" "$@"
+        $reset_umask
+    }
+
+    brew-fix-perms () {
+        local gw="
+            /usr/local/bin
+            /usr/local/include
+            /usr/local/sbin
+            /usr/local/etc
+            /usr/local/Homebrew
+            /usr/local/var
+            /usr/local/var/homebrew
+            /usr/local/var/homebrew/linked
+            /usr/local/lib
+            /usr/local/opt
+            /usr/local/Frameworks
+            /usr/local/share
+        "
+        # shellcheck disable=SC2086
+        chmod 775 $gw
+        chmod -R go=u-w /usr/local
+        find /usr/local \! -perm 444 -a \! -perm 555 -a \! -perm 644 -a \
+            \! -perm 755 -a \! -perm 775 -print0 | \
+            xargs -0 chmod -h go=u-w
+    }
+fi
