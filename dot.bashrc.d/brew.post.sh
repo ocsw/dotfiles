e19d02b86c6f6979aafb6a9b9b3895f50d587965
Danielle Zephyr Malament <danielle.malament@gmail.com>
2019-09-15 04:53:19 -0400

    Remove broken brew perm fixer, add brew-keg-only

--- dot.bashrc.d/brew.post.sh
+++ dot.bashrc.d/brew.post.sh
@@ -12,32 +12,16 @@ if in_path brew; then
         $reset_umask
     }
 
-    brew-fix-perms () {
-        # WARNING: CHANGE THIS IF YOU ADD ANYTHING SECRET TO /usr/local
-        # (unlikely on a Mac)
-        local gw="
-            /usr/local/bin
-            /usr/local/include
-            /usr/local/sbin
-            /usr/local/etc
-            /usr/local/Homebrew
-            /usr/local/var
-            /usr/local/var/homebrew
-            /usr/local/var/homebrew/linked
-            /usr/local/lib
-            /usr/local/opt
-            /usr/local/Frameworks
-            /usr/local/share
-        "
-        # shellcheck disable=SC2086
-        chmod 775 $gw
-        find /usr/local \! -perm -044 -print0 | xargs -0 chmod -h go=u-w
-    }
-
     brew-up () {
         brew update
         brew upgrade
         brew cleanup
         brew doctor
+    }
+
+    brew-keg-only () {
+        brew info --installed --json=v1 | \
+            jq "map(select(.keg_only == true)) | map(.name)" | grep '"' | \
+            sed -e 's/^ *"//' -e 's/",$//' -e 's/"$//'
     }
 fi
