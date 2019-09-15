39600fa7e08db25776a2fac5c04b07fd6811c0ed
Danielle Zephyr Malament <danielle.malament@gmail.com>
2019-09-14 21:57:16 -0400

    Clean up pip completion

--- dot.bashrc.d/python.post.sh
+++ dot.bashrc.d/python.post.sh
@@ -20,12 +20,10 @@ if in_path pip ||
         in_path pip2 ||
         in_path pip3; then
     _pip_completion () {
-        while read -r -a line; do
-            COMPREPLY+=("${line[@]}")
-        done < <(COMP_WORDS="${COMP_WORDS[*]}" \
-                COMP_CWORD="$COMP_CWORD" \
-                PIP_AUTO_COMPLETE=1 "$1")
-        [ -n "$line" ] && COMPREPLY+=("${line[@]}")
+        # shellcheck disable=2207
+        COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+                       COMP_CWORD=$COMP_CWORD \
+                       PIP_AUTO_COMPLETE=1 $1 ) )
     }
     in_path pip && complete -o default -F _pip_completion pip
     in_path pip2 && complete -o default -F _pip_completion pip2
