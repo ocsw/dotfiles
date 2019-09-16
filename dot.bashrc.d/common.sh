a705c6ddb65f097e6979f4b0840ebe20a47d717f
Danielle Zephyr Malament <danielle.malament@gmail.com>
2019-09-15 23:26:30 -0400

    Add a real in_path() function

--- dot.bashrc.d/common.sh
+++ dot.bashrc.d/common.sh
@@ -12,6 +12,23 @@ is_available () {
     hash "$@" > /dev/null 2>&1
 }
 
+# check for a command (or commands) in the PATH, not including aliases or
+# functions; may not be 100% portable
+# note: I can't find a way to get a portable equivalent of 'which' from Bash
+# builtins, but 'command' can at least be used to run a command bypassing
+# aliases and functions
+in_path () {
+    local cmd
+    # don't rely on 'which' accepting multiple commands to search for
+    for cmd in "$@"; do
+        # shellcheck disable=SC2230
+        if ! which "$cmd" > /dev/null 2>&1; then
+            return 1
+        fi
+    done
+    return 0
+}
+
 # check for component of $PATH itself
 is_path_component () {
     [[ $PATH =~ (^|:)$1(:|$) ]]
