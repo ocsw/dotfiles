4befd3bb210853fc25106b6be7eddae437000519
Danielle Zephyr Malament <danielle.malament@gmail.com>
2019-09-18 14:39:28 -0400

    Make gcloud work with python2 venvs

--- dot.bashrc.d/gcloud.post.sh
+++ dot.bashrc.d/gcloud.post.sh
@@ -2,14 +2,20 @@
 
 # see common.sh
 gcloud () {
+    local env_var="CLOUDSDK_PYTHON=python3"
     local arg
+    if ! python3 --version > /dev/null 2>&1; then
+        env_var="CLOUDSDK_PYTHON=python"
+    fi
     for arg in "$@"; do
         if [ "$arg" = "components" ]; then
-            umask_wrap 022 gcloud "$@"
+            umask_wrap 022 env "$env_var" gcloud "$@"
             return "$?"
         fi
     done
-    command gcloud "$@"
+    # env also acts like the 'command' builtin - it bypasses aliases and
+    # functions
+    env "$env_var" gcloud "$@"
 }
 
 # can't use is_available() because we just defined a function with the same name
