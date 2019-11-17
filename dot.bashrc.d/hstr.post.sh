f263c70a4bb7b8129ee3532395f9f7c33e4bfb0f
Danielle Zephyr Malament <danielle.malament@gmail.com>
2019-11-16 23:39:01 -0500

    More hstr work

--- dot.bashrc.d/hstr.post.sh
+++ dot.bashrc.d/hstr.post.sh
@@ -4,18 +4,20 @@ if is_available hstr; then
     HSTR_CONFIG="hicolor"
     HSTR_CONFIG="${HSTR_CONFIG},prompt-bottom"
     HSTR_CONFIG="${HSTR_CONFIG},raw-history-view"
-    #HSTR_CONFIG="${HSTR_CONFIG},duplicates"
+    HSTR_CONFIG="${HSTR_CONFIG},duplicates"
     HSTR_CONFIG="${HSTR_CONFIG},verbose-kill"
     #HSTR_CONFIG="${HSTR_CONFIG},blacklist"
     HSTR_CONFIG="${HSTR_CONFIG},warning"
+    export HSTR_CONFIG
+
+    export HSTR_PROMPT="hstr> "
+
+    [[ $- =~ .*i.* ]] && bind '"\C-r": "\C-ahstr -- \C-j"'
 
     alias hh="hstr"
     alias hhn="hstr -n"
 fi
 
-# TODO
 # ~/.hstr_favorites
 # ~/.hstr_blacklist
-# HSTR_PROMPT
 # export HISTCONTROL=ignorespace
-# if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hstr -- \C-j"'; fi
