34e06e04c1cc6f09e2e72e136424781b69633d27
Danielle Zephyr Malament <danielle.malament@gmail.com>
2020-09-07 03:07:51 -0400

    Add jenv

---
+++ dot.bash_profile.d/java.pre.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if is_available jenv; then
+    if ! is_path_component "${HOME}/.jenv/bin" &&
+            [ -d "${HOME}/.jenv/bin" ]; then
+        export PATH="${HOME}/.jenv/bin:$PATH"
+    fi
+    if ! is_path_component "${HOME}/.jenv/shims" &&
+            [ -d "${HOME}/.jenv/shims" ]; then
+        export PATH="${HOME}/.jenv/shims:$PATH"
+    fi
+fi
---
+++ dot.bashrc.d/java.post.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+if is_available jenv; then
+    eval "$(jenv init - | grep -v "PATH")"
+fi
