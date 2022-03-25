8710847d74af8d89c5bdb1242b274e487fa15f8a
Danielle Zephyr Malament <danielle.malament@gmail.com>
2022-03-25 18:34:21 -0400

    Add nvm

--- dot.bashrc.d/node.post.sh
+++ dot.bashrc.d/node.post.sh
@@ -19,3 +19,13 @@ npm () {
     fi
     command npm "$@"
 }
+
+if [ -d "$HOME/.nvm" ]; then
+    export NVM_DIR="$HOME/.nvm"
+    # shellcheck disable=SC1091
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && \
+        . "/usr/local/opt/nvm/nvm.sh"
+    # shellcheck disable=SC1091
+    [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \
+        . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+fi
