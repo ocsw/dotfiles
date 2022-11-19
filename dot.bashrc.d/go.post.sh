12189e447a8c6f31e7439b8a26ca56105ca8d467
Danielle Zephyr Malament <danielle.malament@gmail.com>
2022-11-19 00:36:47 -0500

    Add VSCode Go build tags to function

--- dot.bashrc.d/go.post.sh
+++ dot.bashrc.d/go.post.sh
@@ -6,4 +6,17 @@ vscode-golang-settings () {
     # this removes highlighting of tabs (there doesn't seem to be a way to do it
     # only for .go files)
     vscode-setting -j "highlight-bad-chars.additionalUnicodeChars" '[""]'
+
+    local tags="$1"
+    if [ -n "$tags" ]; then
+        vscode-setting -j "go.buildFlags" "[\"-tags=${tags}\"]"
+        vscode-setting -j "go.toolsEnvVars" "{\"GOTAGS\": \"${tags}\"}"
+        vscode-setting -j "go.lintFlags" "[
+            \"-E\", \"exportloopref,goimports,lll,revive,whitespace\",
+            \"-E\", \"stylecheck\",
+            \"--max-issues-per-linter\", \"0\", \"--max-same-issues\", \"0\",
+            \"--fast\",
+            \"--build-tags\", \"${tags}\"
+        ]"
+    fi
 }
