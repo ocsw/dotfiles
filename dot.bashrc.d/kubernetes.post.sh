24eaf98b0c13090e4d9a7d96a0454eb63e839409
Danielle Zephyr Malament <danielle.malament@gmail.com>
2023-04-10 16:36:18 -0400

    Wrap GKE var in ifs

--- dot.bashrc.d/kubernetes.post.sh
+++ dot.bashrc.d/kubernetes.post.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+# can't use is_available() for gcloud because we defined a function with the
+# same name
+if in_path gcloud && is_available kubectl; then
+    export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+fi
