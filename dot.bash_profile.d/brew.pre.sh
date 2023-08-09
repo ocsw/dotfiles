41beb4f8418d154c7760375b66ade63b8fc0e0d2
Danielle Zephyr Malament <danielle.malament@gmail.com>
2023-08-09 15:02:44 -0400

    Brew stuff for M2

---
+++ dot.bash_profile.d/brew.pre.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# paths on Apple Silicon
+if [ "$(uname)" = "Darwin" ] && [ -d "/opt/homebrew" ]; then
+    export HOMEBREW_PREFIX="/opt/homebrew"
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar"
+    export HOMEBREW_REPOSITORY="/opt/homebrew"
+    if ! is_path_component "${HOMEBREW_PREFIX}/bin"; then
+        export PATH="${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin:${PATH}"
+    fi
+    if ! [[ $MANPATH =~ (^|:)$HOMEBREW_PREFIX/share/man(:|$) ]]; then
+        # need the trailing : if there is no MANPATH
+        export MANPATH="${HOMEBREW_PREFIX}/share/man:${MANPATH}"
+    fi
+    if ! [[ $INFOPATH =~ (^|:)$HOMEBREW_PREFIX/share/info(:|$) ]]; then
+        export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH}"
+    fi
+fi
