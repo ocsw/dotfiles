abb7ec13922bca7d2b4136b686d85515bc6439b3
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-09-21 14:18:47 -0400

    Add VSCode config checker

--- dot.bashrc.d/vscode.post.sh
+++ dot.bashrc.d/vscode.post.sh
@@ -2,6 +2,21 @@
 
 # (see also go.post.sh)
 
+vscode-check-config () {
+    diff "${SYSTEM_SETUP}/vscode/extensions.txt" \
+        <(jq -r '.[].identifier.id' < \
+            "${HOME}/.vscode/extensions/extensions.json" | sort -t. -k2 -u)
+
+    if [ "$(uname)" = "Darwin" ]; then
+        local USER_CONFIG_DIR="${HOME}/Library/Application Support/Code/User"
+        diff "${SYSTEM_SETUP}/vscode/settings.json" \
+            "${USER_CONFIG_DIR}/settings.json"
+        diff "${SYSTEM_SETUP}/vscode/keybindings.json" \
+            "${USER_CONFIG_DIR}/keybindings.json"
+    fi
+}
+
+
 _vscode-setting-usage () {
     cat 1>&2 <<EOF
 Usage:
