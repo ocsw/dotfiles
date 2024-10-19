980d61fd0030512437382f079bf3506ef34ac221
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-10-19 00:17:00 -0400

    Add KUBE_DEBUG_IMAGE

--- dot.bashrc.d/kubernetes.post.sh
+++ dot.bashrc.d/kubernetes.post.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-# can't use is_available() for gcloud because we defined a function with the
-# same name
-if in_path gcloud && is_available kubectl; then
-    export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+if is_available kubectl; then
+    # can't use is_available() for gcloud because we defined a function with
+    # the same name
+    if in_path gcloud; then
+        export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+    fi
+
+    # Docker image for use with e.g. kubectl debug
+    # shellcheck disable=SC2034
+    KUBE_DEBUG_IMAGE="nicolaka/netshoot"
 fi
