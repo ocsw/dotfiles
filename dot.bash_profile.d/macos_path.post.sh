e77fb8e7bba3f4b7d35604f2261ae45ce027430d
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-10-27 13:27:38 -0400

    Make Mac path sub more robust

--- dot.bash_profile.d/macos_path.post.sh
+++ dot.bash_profile.d/macos_path.post.sh
@@ -2,5 +2,5 @@
 
 if [ "$(uname)" = "Darwin" ] && [ -d /usr/local/sbin ] &&
         ! is_path_component "/usr/local/sbin" ; then
-    export PATH="${PATH//\/usr\/sbin://usr/local/sbin:/usr/sbin:}"
+    export PATH="${PATH//:\/usr\/sbin:/:/usr/local/sbin:/usr/sbin:}"
 fi
