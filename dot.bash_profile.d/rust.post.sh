bc78fde78837d0b3eed5c38fbca9222f1703ce31
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-11-02 18:02:52 -0400

    Clean up rustc test

--- dot.bash_profile.d/rust.post.sh
+++ dot.bash_profile.d/rust.post.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -f "${HOME}/.cargo/bin/rustc" ] ||
-        is_available rustc && [ -d "${HOME}/.cargo/bin" ]; then
-    if ! is_path_component "${HOME}/.cargo/bin"; then
+if is_available rustc || [ -f "${HOME}/.cargo/bin/rustc" ]; then
+    if ! is_path_component "${HOME}/.cargo/bin" && \
+            [ -d "${HOME}/.cargo/bin" ]; then
         export PATH="${PATH}:${HOME}/.cargo/bin"
     fi
 fi
