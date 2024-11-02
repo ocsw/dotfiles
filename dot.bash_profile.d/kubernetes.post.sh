bbb369104f599a459fe99db57ebf90eb387dcb12
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-11-02 16:58:03 -0400

    Move gcloud GKE env var to bash_profile and add links between the k8s files

--- dot.bash_profile.d/kubernetes.post.sh
+++ dot.bash_profile.d/kubernetes.post.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# See also ../dot.bashrc.d/kubernetes.post.sh
+
+# Can't use is_available() for gcloud because we define a function with the
+# same name later and we might be re-sourcing this
+if is_available kubectl && in_path gcloud; then
+    export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+fi
+
 if kubectl krew version > /dev/null 2>&1; then
     if ! is_path_component "${HOME}/.krew/bin" &&
             [ -d "${HOME}/.krew/bin" ]; then
--- dot.bashrc.d/kubernetes.post.sh
+++ dot.bashrc.d/kubernetes.post.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-if is_available kubectl; then
-    # can't use is_available() for gcloud because we defined a function with
-    # the same name
-    if in_path gcloud; then
-        export USE_GKE_GCLOUD_AUTH_PLUGIN=True
-    fi
+# See also ../dot.bash_profile.d/kubernetes.post.sh
 
+if is_available kubectl; then
     # Docker image for use with e.g. kubectl debug
     # shellcheck disable=SC2034
     KUBE_DEBUG_IMAGE="nicolaka/netshoot"
