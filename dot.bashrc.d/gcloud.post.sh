bdefe8b81b5e2853e9e5ee92515c2dd0e413e473
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-11-02 16:36:56 -0400

    Don't really need CLOUDSDK_PYTHON for gcloud anymore

--- dot.bashrc.d/gcloud.post.sh
+++ dot.bashrc.d/gcloud.post.sh
@@ -2,26 +2,18 @@
 
 # see common.sh
 gcloud () {
-    local env_var="CLOUDSDK_PYTHON=python3"
     local arg
-    if ! python3 --version > /dev/null 2>&1; then
-        env_var="CLOUDSDK_PYTHON=python"
-    fi
     for arg in "$@"; do
         if [ "$arg" = "components" ]; then
-            umask_wrap 022 env "$env_var" gcloud "$@"
+            umask_wrap 022 gcloud "$@"
             return "$?"
         fi
     done
-    # env also acts like the 'command' builtin - it bypasses aliases and
-    # functions
-    env "$env_var" gcloud "$@"
+    command gcloud "$@"
 }
 
 # can't use is_available() because we defined a function with the same name
 if in_path gcloud; then
-    export CLOUDSDK_PYTHON=python3
-
     gcloud-up () {
         gcloud components update
     }
