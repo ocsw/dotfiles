f4fdae9a0baaf42e68b5882923922bd204442dad
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-11-02 17:44:48 -0400

    Move chef exports from bashrc to bash_profile

--- dot.bash_profile.d/chef.pre.sh
+++ dot.bash_profile.d/chef.pre.sh
@@ -4,6 +4,7 @@
 
 if is_available chef; then
     if ! [[ $PATH =~ /.chef/ ]]; then
-        eval "$(chef shell-init "$(basename "$SHELL")" | grep 'export PATH=')"
+        eval "$(chef shell-init "$(basename "$SHELL")" | grep '^export ')"
     fi
+    export CHEF_LICENSE=accept-silent
 fi
--- dot.bashrc.d/chef.post.sh
+++ dot.bashrc.d/chef.post.sh
@@ -3,6 +3,5 @@
 # See also ../dot.bash_profile.d/chef.pre.sh
 
 if is_available chef; then
-    eval "$(chef shell-init "$(basename "$SHELL")" | grep -v 'export PATH=')"
-    export CHEF_LICENSE=accept-silent
+    eval "$(chef shell-init "$(basename "$SHELL")" | grep -v '^export ')"
 fi
