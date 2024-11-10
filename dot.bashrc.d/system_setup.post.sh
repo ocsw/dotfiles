33fa07d84d0ceb4f2f8d5dd252164b9cb9a7f330
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-11-10 14:42:36 -0500

    Add back links from system_setup_post.sh

--- dot.bashrc.d/system_setup.post.sh
+++ dot.bashrc.d/system_setup.post.sh
@@ -2,5 +2,6 @@
 
 :  # noop; prevents shellcheck disable from covering the whole file
 # See https://github.com/ocsw/system-setup
+# (See also git.post.sh, updates.post.sh, and vscode.post.sh)
 # shellcheck disable=SC2034
 SYSTEM_SETUP="${HOME}/repos/system-setup"
