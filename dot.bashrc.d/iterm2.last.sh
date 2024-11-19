1f1b2c38e5bd48c8b95f09675ffbdb3666011c25
Danielle Zephyr Malament <danielle.malament@gmail.com>
2024-11-19 01:26:31 -0500

    Clean up organization of iTerm2 stuff a little bit

--- dot.bashrc.d/iterm2.last.sh
+++ dot.bashrc.d/iterm2.last.sh
@@ -2,16 +2,18 @@
 
 # See also ../dot.bash_profile.d/iterm2.post.sh
 
-# See https://iterm2.com/documentation-shell-integration.html and
-# https://github.com/gnachman/iTerm2-shell-integration/tree/main/shell_integration
-ITERM2_BASH_INTEGRATION="/Applications/iTerm.app/Contents/Resources/iterm2_shell_integration.bash"
-
-if [ "$TERM_PROGRAM" = iTerm.app ]; then
+if [ "$TERM_PROGRAM" = "iTerm.app" ]; then
+    # See https://iterm2.com/documentation-shell-integration.html
+    #
+    # Source is in
+    # https://github.com/gnachman/iTerm2-shell-integration/tree/main/shell_integration
+    ITERM2_BASH_INTEGRATION_PATH="/Applications/iTerm.app/Contents/Resources/iterm2_shell_integration.bash"
+    #
     # ITERM_SHELL_INTEGRATION_INSTALLED is set by the integration script, at
     # least as of v18
     # shellcheck disable=SC1090
     if [ -z "$ITERM_SHELL_INTEGRATION_INSTALLED" ] &&
-            [ -e "$ITERM2_BASH_INTEGRATION" ]; then
-        . "$ITERM2_BASH_INTEGRATION"
+            [ -e "$ITERM2_BASH_INTEGRATION_PATH" ]; then
+        . "$ITERM2_BASH_INTEGRATION_PATH"
     fi
 fi
