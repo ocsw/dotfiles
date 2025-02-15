b052f288820b3f798d3ca4629aa593e502b71a56
Danielle Zephyr Malament <danielle.malament@gmail.com>
2025-02-15 17:41:23 -0500

    Clearer formatting in INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+#
+# General setup
+#
+
 umask 077
 
 mkdir -p "${HOME}/bin"
+
+
+#
+# Clone repos
+#
+
 mkdir -p "${HOME}/repos"
 
 DOTFILE_REPO="${DOTFILE_REPO:-${HOME}/repos/dotfiles}"
@@ -20,6 +30,11 @@ git clone git@github.com:ocsw/dotfiles.git "$DOTFILE_REPO"
 git clone git@github.com:ocsw/pypvutil.git "$PYPVUTIL_REPO"
 git clone git@github.com:ocsw/system-setup.git "$SYSTEM_SETUP"
 
+
+#
+# Link/copy dotfiles from repo to $HOME
+#
+
 # See https://github.com/ocsw/dotfiles/blob/main/dot.bashrc.d/dotfiles.post.sh
 # shellcheck disable=SC1091
 . "${DOTFILE_REPO}/dot.bashrc.d/dotfiles.post.sh"
@@ -32,19 +47,24 @@ ln_dotfile .inputrc
 cp_dotfile .bashrc.local  # copy only
 #ln_dotfile .bash_logout  # maybe
 ln_dotfile .hstr_blacklist
-#
+
 ln_dotfile .vimrc
 ln_dotfile .vimrc.d
 cp_dotfile .vimrc.local  # copy only
-#
+
 #ln_dotfile .muttrc  # maybe
-#
+
 if [ "$(uname)" = "Darwin" ]; then
     mkdir -p "${HOME}/.hammerspoon"
     ln -s "${DOTFILE_REPO}/hammerspoon/init.lua" "${HOME}/.hammerspoon/"
     ln -s "${DOTFILE_REPO}/hammerspoon/hammers" "${HOME}/.hammerspoon/"
     cp "${DOTFILE_REPO}/hammerspoon/init-local.lua" "${HOME}/.hammerspoon/"
 fi
+
+
+#
+# Move and link dotfiles for easier backups
+#
 
 # See https://github.com/ocsw/dotfiles/blob/main/dot.bashrc.d/to_back_up.post.sh
 # shellcheck disable=SC1091
@@ -58,9 +78,10 @@ touch .bashrc.local
 ln_tbu .bashrc.local
 touch .hstr_favorites
 ln_tbu .hstr_favorites
+
 touch .vimrc.local
 ln_tbu .vimrc.local
-#
+
 mkdir -p .ssh
 chmod 700 .ssh
 touch .ssh/config
@@ -69,7 +90,7 @@ ln_tbu .ssh/config
 touch .ssh/known_hosts
 chmod 600 .ssh/known_hosts
 ln_tbu .ssh/known_hosts
-#
+
 mkdir -p .gnupg
 touch .gnupg/dirmngr.conf
 ln_tbu .gnupg/dirmngr.conf
@@ -86,7 +107,7 @@ ln_tbu .gnupg/gpg.conf
 #     touch .gnupg_pre_2.1/gpg.conf
 #     ln_tbu .gnupg_pre_2.1/gpg.conf
 # fi
-#
+
 touch .gitconfig
 ln_tbu .gitconfig
 touch .gitconfig_rewrites
@@ -96,7 +117,7 @@ while IFS= read -r file; do
 done < <(compgen -G ".gitconfig_*")
 touch .gitignore_global
 ln_tbu .gitignore_global
-#
+
 mkdir -p .ipython
 chmod 700 .ipython
 ln_tbu .ipython
@@ -109,13 +130,13 @@ touch .pypirc
 ln_tbu .pypirc
 touch .flake8
 ln_tbu .flake8
-#
+
 if [ "$(uname)" = "Darwin" ]; then
     mkdir -p .hammerspoon
     touch .hammerspoon/init-local.lua
     ln_tbu .hammerspoon/init-local.lua
 fi
-#
+
 vscode_confdir=""
 if [ "$(uname)" = "Darwin" ]; then
     vscode_confdir="Library/Application Support/Code/User"
