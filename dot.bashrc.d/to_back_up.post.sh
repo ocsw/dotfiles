cd5a0bdd48d8742b6fa7de8ad8b15f01b02c2ac6
Danielle Zephyr Malament <danielle.malament@gmail.com>
2026-05-02 13:09:46 -0400

    Make ln_tbu accept a BACKUP_DIR; increase verbosity

--- dot.bashrc.d/to_back_up.post.sh
+++ dot.bashrc.d/to_back_up.post.sh
@@ -4,10 +4,10 @@ TBU_DIR="${HOME}/.to_back_up"
 
 _ln_tbu_usage () {
     cat <<EOF
-Usage: ln_tbu SOURCE_PATH
+Usage: ln_tbu SOURCE_PATH [BACKUP_DIR]
 
-Moves the file/directory/etc. at the SOURCE_PATH to the backup directory set in
-\$TBU_DIR and replaces it with a symlink to the new location.  The idea is to
+Moves the file/directory/etc. at the SOURCE_PATH to the backup directory
+BACKUP_DIR and replaces it with a symlink to the new location.  The idea is to
 put things like config files in a single place that can be easily added to
 backup or sync software.
 
@@ -16,17 +16,17 @@ directories matching the source's path, with one exception: if the orignal path
 starts with the value of \$HOME, it will be stripped first.
 
 For example, ~/.config/app/config.json will me moved to
-\$TBU_DIR/.config/app/.config.json.
+BACKUP_DIR/.config/app/.config.json.
 
 This makes it easier to work with the files in the backup directory, but it
 does mean that this function will group together items from \$HOME and / with
 the same first path component, and it will fail if they have the same
 destinations.  However, that's unlikely to come up in practice.
 
-SOURCE_PATH may not contain any .. elements.
+SOURCE_PATH may not contain any .. elements and must already exist.
 
-TBU_DIR must be an absolute path, and it will be created if it doesn't
-exist.
+BACKUP_DIR must be an absolute path, and it will be created if it doesn't
+exist.  It defaults to the value of \$TBU_DIR.
 
 This function will refuse to move the source if the destination already exists,
 so it's safe to run it multiple times with the same source.
@@ -35,31 +35,17 @@ EOF
 
 ln_tbu () {
     local source="$1"
+    local backup_dir="$2"
+    local err
     local source_clean
     local source_stripped
     local subtree
     local source_name
     local destination
 
-    if [ -z "$TBU_DIR" ]; then
-        echo "ERROR: \$TBU_DIR must contain the path to the backup directory." \
-            1>&2
-        return 1
-    fi
-
-    if ! [[ $TBU_DIR =~ ^/ ]]; then
-        echo "ERROR: \$TBU_DIR must be an absolute path." 1>&2
-        return 1
-    fi
-
-    if ! [ -d "$TBU_DIR" ]; then
-        if ! mkdir -p "$TBU_DIR"; then
-            echo 1>&2
-            printf "%s\n" \
-                "ERROR: Can't create backup directory '$TBU_DIR'" 1>&2
-            echo 1>&2
-            return 1
-        fi
+    if [ "$source" = "-h" ] || [ "$source" = "--help" ] ; then
+        _ln_tbu_usage
+        return
     fi
 
     if [ -z "$source" ]; then
@@ -71,16 +57,48 @@ ln_tbu () {
 
     # I'm not going to mess with resolving '..', so just break
     if [[ $source =~ ^\.\./ ]] || [[ $source =~ /\.\.$ ]] ||
-        [[ $source =~ /\.\./ ]]; then
-        _ln_tbu_usage 1>&2
+        [[ $source =~ /\.\./ ]] || [ "$source" = ".." ]; then
         echo 1>&2
-        echo "ERROR: Source path contains .." 1>&2
+        echo "ERROR: Source path contains a '..' element." 1>&2
+        echo 1>&2
         return 1
     fi
 
     if ! [ -e "$source" ]; then
+        echo 1>&2
         echo "ERROR: Source doesn't exist." 1>&2
+        echo 1>&2
         return 1
+    fi
+
+    if [ -z "$backup_dir" ]; then
+        #echo "No BACKUP_DIR given; defaulting to \$TBU_DIR ('$TBU_DIR')."
+
+        if [ -z "$TBU_DIR" ]; then
+            echo 1>&2
+            echo "ERROR: \$TBU_DIR is unset or empty." 1>&2
+            echo 1>&2
+            return 1
+        fi
+
+        backup_dir="$TBU_DIR"
+    fi
+
+    if ! [[ $backup_dir =~ ^/ ]]; then
+        echo 1>&2
+        echo "ERROR: BACKUP_DIR must be an absolute path." 1>&2
+        echo 1>&2
+        return 1
+    fi
+
+    if ! [ -d "$backup_dir" ]; then
+        printf "%s\n" "Creating backup directory..."
+        if ! err=$(mkdir -p "$backup_dir" 2>&1); then
+            echo 1>&2
+            printf "%s\n" "ERROR: $err" 1>&2
+            echo 1>&2
+            return 1
+        fi
     fi
 
     #
@@ -92,18 +110,45 @@ ln_tbu () {
     #
     # Strip out '.' elements from the path and condense multiple slashes
     source_clean=$(printf "%s\n" "$source" |
-        sed -e 's|^\./||' -e 's|/\.$||' -e 's|/\./|/|g' -e 's|///*|/|g')
+        sed -e 's|^\./||' -e 's|/\.$|/|' -e 's|/\./|/|g' -e 's|^\.$||' \
+            -e 's|///*|/|g')
 
     # Make relative paths absolute
     if ! [[ $source_clean =~ ^/ ]]; then
         source_clean="${PWD}/${source_clean}"
     fi
 
-    # Figure out the subtree under $TBU_DIR; first, remove $HOME as a prefix,
-    # then leading and trailing slashes
+    # Remove the trailing slash, if present.  Note that if this is left in
+    # place, ln will later fail because it will think we want it to make the
+    # symlink inside the source directory, which will already have been moved.
+    source_clean="${source_clean%/}"
+
+    # If there's nothing left, we were being asked to operate on /
+    # (Wait until this point to check so that we've already cleaned up, e.g.,
+    # '/.')
+    if [ -z "$source_clean" ]; then
+        echo 1>&2
+        echo "ERROR: Source cannot be '/'." 1>&2
+        echo 1>&2
+        return 1
+    fi
+
+    # Figure out the subtree under $backup_dir; first, remove $HOME as a
+    # prefix
+    # (See https://www.shellcheck.net/wiki/SC2295 about the quotes)
     source_stripped="${source_clean#"$HOME"}"
+
+    # If there's nothing left, we were being asked to operate on $HOME
+    # (Wait until this point to check so that we've already cleaned up, e.g.,
+    # '~/.')
+    # (That's actually fine; the mv will fail later if the BACKUP_DIR is inside
+    # $HOME, but otherwise, we can just treat it like a regular absolute path)
+    if [ -z "$source_stripped" ]; then
+        source_stripped="$HOME"
+    fi
+
+    # Strip the leading slash if there is one
     source_stripped="${source_stripped#/}"
-    source_stripped="${source_stripped%/}"
 
     # Now we need to split what's left into a subtree and a name
     if ! [[ $source_stripped =~ / ]]; then
@@ -114,31 +159,42 @@ ln_tbu () {
         source_name="${source_stripped##*/}"
     fi
 
-    if [ -n "$subtree" ]; then
-        if ! mkdir -p "${TBU_DIR}/${subtree%/}"; then
+    if [ -n "$subtree" ] && ! [ -d "${backup_dir}/${subtree%/}" ]; then
+        printf "%s\n" "Creating subtree '$subtree' under backup directory..."
+        if ! err=$(mkdir -p "${backup_dir}/${subtree%/}" 2>&1); then
             echo 1>&2
-            printf "%s\n" \
-                "ERROR: Can't create subtree '$subtree' in backup directory '$TBU_DIR'." \
-                1>&2
+            printf "%s\n" "ERROR: $err" 1>&2
             echo 1>&2
             return 1
         fi
     fi
 
-    destination="${TBU_DIR}/${subtree}${source_name}"
+    destination="${backup_dir}/${subtree}${source_name}"
+    printf "%s\n" "Source: $source_clean"
+    printf "%s\n" "Destination: $destination"
 
     if [ -e "$destination" ]; then
-        printf "%s\n" "ERROR: Destination '$destination' already exists." 1>&2
+        echo 1>&2
+        printf "%s\n" "ERROR: Destination already exists." 1>&2
+        echo 1>&2
         return 1
     fi
 
-    if ! mv "$source" "$destination"; then
+    printf "%s\n" "Moving source to destination..."
+    if ! err=$(mv "$source_clean" "$destination" 2>&1); then
         echo 1>&2
-        printf "%s\n" \
-            "ERROR: Can't move source to backup destination '$destination'." \
-            1>&2
+        printf "%s\n" "ERROR: $err" 1>&2
         echo 1>&2
         return 1
     fi
-    ln -s "$destination" "$source"
+
+    printf "%s\n" "Linking source to destination..."
+    if ! err=$(ln -s "$destination" "$source_clean" 2>&1); then
+        echo 1>&2
+        printf "%s\n" "ERROR: $err" 1>&2
+        echo 1>&2
+        return 1
+    fi
+
+    echo "Done."
 }
